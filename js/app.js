ba15f0da3b8daae29c9d71714d8495eeeaadc09c
rafalcl <rafalclab@gmail.com>
2018-07-27 14:17:09 +0200

    Initialize project + zadanie rozgrzewkowe

---
+++ js/app.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+
+//  $.ajax({
+//    url:"http://date.jsontest.com",
+//  }).done(function(sentDate){
+//    console.log("Serwer wysłał odpowiedź. Poniżej wartość.");
+//    console.log(sentDate);
+//  }).fail(function(xhr, status, errorThrown){
+//    console.log(xhr, status, errorThrown);
+//  });
+//
+//  $.ajax({
+//    url:"https://swapi.co/api/people/4/",
+//  }).done(function(response){
+//    console.log("Serwer wysłał odpowiedź. Poniżej wartość.");
+//    console.log(response);
+//  }).fail(function(xhr, status, errorThrown){
+//    console.log(xhr, status, errorThrown);
+//  });
+
+});
