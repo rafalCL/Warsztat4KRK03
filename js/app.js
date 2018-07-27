185b3f791061801642ba7733dd5ad6827e3f445b
rafalcl <rafalclab@gmail.com>
2018-07-27 14:38:24 +0200

    Render book list

--- js/app.js
+++ js/app.js
@@ -1,5 +1,33 @@
 $(document).ready(function(){
 
+  refreshBookList();
+
+  function refreshBookList(){
+    $.ajax({url: "http://localhost:8282/books/",
+           }).done(function(books){
+      console.log("Działa! :)");
+      var renderingPoint = $("#book-list");
+      renderBookList(renderingPoint, books);
+    }).fail(function(xhr, status, errorThrown){
+      console.log("BŁĄD!", xhr, status, errorThrown);
+    });
+  }
+
+  function renderBookList(renderingPoint, books){
+    var ul = $("<ul>");
+    for(var i=0; i< books.length; i++){
+      var id = books[i].id;
+      var title = books[i].title;
+      var author = books[i].author;
+
+      var li = $("<li>");
+      li.text(id + "; " + title + "; " + author);
+      ul.append(li);
+    }
+
+    renderingPoint.append(ul);
+  }
+
 //  $.ajax({
 //    url:"http://date.jsontest.com",
 //  }).done(function(sentDate){
